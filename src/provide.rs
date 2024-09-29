579d6ff44d211846f8b704fce375ad56823bcb57
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2024-09-29 16:17:32 +0700

    feat: more Provide impls for syn_2

--- src/provide.rs
+++ src/provide.rs
@@ -1,5 +1,7 @@
 #[cfg(feature = "std")]
 use std::path::{Path, PathBuf};
+#[cfg(feature = "syn_2")]
+use syn_2::LifetimeParam;
 
 /// `Provide` is similar to `AsRef`, `Into`, `TryInto`. However, `Provide` allows specifying the return type, so the implementor may choose to return a `T`, `&T`, `Option<T>`, `Result<T, ...>`.
 ///
@@ -16,5 +18,19 @@ impl Provide<PathBuf> for &Path {
 
     fn provide(self) -> PathBuf {
         self.to_path_buf()
+    }
+}
+
+#[cfg(feature = "syn_2")]
+impl Provide<LifetimeParam> for syn_2::Lifetime {
+    type Output = LifetimeParam;
+
+    fn provide(self) -> LifetimeParam {
+        LifetimeParam {
+            attrs: vec![],
+            lifetime: self,
+            colon_token: None,
+            bounds: Default::default(),
+        }
     }
 }

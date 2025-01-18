344683a442bd07f3ac4d83d0856304b831f2b3fd
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-01-18 20:42:13 +0700

    feat: impl IsUppercase

--- src/is_uppercase.rs
+++ src/is_uppercase.rs
@@ -1,3 +1,5 @@
+use alloc::string::String;
+
 pub trait IsUppercase {
     fn is_uppercase(&self) -> bool;
 }
@@ -5,5 +7,17 @@ pub trait IsUppercase {
 impl IsUppercase for char {
     fn is_uppercase(&self) -> bool {
         char::is_uppercase(*self)
+    }
+}
+
+impl IsUppercase for str {
+    fn is_uppercase(&self) -> bool {
+        self.chars().all(char::is_uppercase)
+    }
+}
+
+impl IsUppercase for String {
+    fn is_uppercase(&self) -> bool {
+        self.chars().all(char::is_uppercase)
     }
 }
